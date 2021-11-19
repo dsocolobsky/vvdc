efdb27872ac65b91205155324cda12a028087153
Dylan Socolobsky <dsocolobsky@gmail.com>
2021-11-19 20:28:14 -0300

    move parse expression logic to separate function

--- src/parser.rs
+++ src/parser.rs
@@ -40,53 +40,61 @@ impl Expression {
     }
 }
 
+pub fn parse_expression(tokens: &Vec<Token>, from: usize) -> (Option<Expression>, usize) {
+    let token = &tokens[from];
+    match token.token_type {
+        TokenType::Bang => {
+            let expression = Expression::prefix_expression(
+                tokens[from].clone(),
+                Expression::literal_expression(tokens[from+1].clone()
+            ));
+            return (Some(expression), 2)
+        },
+        TokenType::String | TokenType::Literal  | TokenType::Number => {
+            let expression = Expression::literal_expression(tokens[from].clone());
+            return (Some(expression), 1)
+        },
+        TokenType::Assignment => todo!(),
+        TokenType::Plus => todo!(),
+        TokenType::Minus => todo!(),
+        TokenType::Asterisk => todo!(),
+        TokenType::Semicolon => {
+            return (None, 1)
+        },
+        TokenType::Equals => todo!(),
+        TokenType::Unequal => todo!(),
+        TokenType::Lt => todo!(),
+        TokenType::Gt => todo!(),
+        TokenType::Lteq => todo!(),
+        TokenType::Gteq => todo!(),
+        TokenType::Lparen => todo!(),
+        TokenType::Rparen => todo!(),
+        TokenType::Lbrace => todo!(),
+        TokenType::Rbrace => todo!(),
+        TokenType::KeywordIf => todo!(),
+        TokenType::KeywordPrint => todo!(),
+        TokenType::KeywordReturn => {
+            let expression = Expression::return_expression(
+                Expression::literal_expression(tokens[from+1].clone())
+            ); 
+            return (Some(expression), 2)
+        },
+        TokenType::KeywordWhile => todo!(),
+        TokenType::KeywordLet => todo!(),
+        TokenType::KeywordFn => todo!(),
+    }
+}
+
 pub fn parse(tokens: &Vec<Token>) ->Vec<Expression> {
     let mut expressions = Vec::new();
     let mut index: usize = 0;
 
     while index < tokens.len() {
-        let token = &tokens[index];
-        match token.token_type {
-            TokenType::Bang => {
-                expressions.push(Expression::prefix_expression(
-                    tokens[index].clone(),
-                    Expression::literal_expression(tokens[index+1].clone()
-                )));
-                index = index + 2;
-            },
-            TokenType::String | TokenType::Literal  | TokenType::Number => {
-                expressions.push(Expression::literal_expression(tokens[index].clone()));
-                index = index + 1;
-            },
-            TokenType::Assignment => todo!(),
-            TokenType::Plus => todo!(),
-            TokenType::Minus => todo!(),
-            TokenType::Asterisk => todo!(),
-            TokenType::Semicolon => {
-                index = index + 1;
-            },
-            TokenType::Equals => todo!(),
-            TokenType::Unequal => todo!(),
-            TokenType::Lt => todo!(),
-            TokenType::Gt => todo!(),
-            TokenType::Lteq => todo!(),
-            TokenType::Gteq => todo!(),
-            TokenType::Lparen => todo!(),
-            TokenType::Rparen => todo!(),
-            TokenType::Lbrace => todo!(),
-            TokenType::Rbrace => todo!(),
-            TokenType::KeywordIf => todo!(),
-            TokenType::KeywordPrint => todo!(),
-            TokenType::KeywordReturn => {
-                expressions.push(Expression::return_expression(
-                    Expression::literal_expression(tokens[index+1].clone())
-                ));
-                index = index + 2;
-            },
-            TokenType::KeywordWhile => todo!(),
-            TokenType::KeywordLet => todo!(),
-            TokenType::KeywordFn => todo!(),
-        };
+        let (expression, advance) = parse_expression(tokens, index);
+        if let Some(exp) = expression {
+            expressions.push(exp);
+        }
+        index = index + advance;
     }
     
     expressions
