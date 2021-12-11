8a477c1cbf1a0683b58cbeb1be4703f782453537
Dylan Socolobsky <dsocolobsky@gmail.com>
2021-12-11 20:13:20 -0300

    Pass program as argument to vvdc; run program automatically, better command outputs.

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,23 @@ pub mod codegen;
 pub mod lexer;
 pub mod parser;
 pub mod tokens;
-use std::fs;
-use std::process::{Command, ExitStatus};
+use std::{env, fs};
+use std::path::Path;
+use std::process::{Command};
 
 fn main() {
-    let code = fs::read_to_string("programs/2_return_addition.vvdl").expect("Unable to read file");
+    let args: Vec<String> = env::args().collect();
+    let program_path = if args.len() > 1 {
+        args[1].clone()
+    } else {
+        "programs/test.vvdl".to_string()
+    };
+    let program_name = Path::new(&program_path).file_stem().unwrap().to_str().unwrap();
+    println!("Compiling {}", program_path);
+    println!("Program name: {}", program_name);
+
+    let code = fs::read_to_string(program_path.clone()).expect("Unable to read file");
+    println!("============ CODE ============");
     println!("{}", code);
     println!("============ TOKENS ============");
     let tokens = lexer::lex_program(&code);
@@ -20,11 +32,12 @@ fn main() {
 
     println!("============ BUILDING ============");
     // Write .asm file
-    fs::write("programs/obj/output.asm", code).expect("Unable to write file");
+    let asm_path = format!("programs/obj/{}.asm", program_name);
+    fs::write(asm_path.clone(), code).expect("Unable to write file");
 
     // Call nasm on .asm file to generate .o file
     let nasm_output = Command::new("nasm")
-        .args(["-f elf64", "programs/obj/output.asm"])
+        .args(["-f elf64", &*asm_path])
         .status()
         .expect("failed to execute nasm");
     if nasm_output.success() {
@@ -34,8 +47,10 @@ fn main() {
     }
 
     // Call linker to generate final executable
+    let executable_path = format!("programs/obj/{}", program_name);
+    let obj_path = format!("{}.o", executable_path);
     let ld_output = Command::new("ld")
-        .args(["-s", "-o", "programs/obj/output", "programs/obj/output.o"])
+        .args(["-s", "-o", &*executable_path, &*obj_path])
         .status()
         .expect("failed to execute ld");
     if ld_output.success() {
@@ -43,6 +58,13 @@ fn main() {
     } else {
         println!("ld error: {}", ld_output);
     }
+
+    println!("============ RUNNING ============");
+    println!("running: {}", executable_path);
+    let obj_output = Command::new(format!("./{}", executable_path))
+        .status()
+        .expect("failed to run compiled program");
+    println!("output: {}", obj_output);
 }
 
 mod test {
